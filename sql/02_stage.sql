6ffc6da8b7f00a71e1d7409771ed139dd343b8a7
JadeMR15 <jade.merle-remond@esme.fr>
2026-05-15 19:17:51 +0200

    Update 02_stage.sql

--- sql/02_stage.sql
+++ sql/02_stage.sql
@@ -1,7 +1,11 @@
-USE DATABASE LINKEDIN;
+-- On se place dans la bonne base et le bon schémaUSE DATABASE LINKEDIN;
 USE SCHEMA BRONZE;
 
+-- Création du stage vers le bucket S3 public
+-- Le stage permet à Snowflake d'accéder aux fichiers distants
 CREATE OR REPLACE STAGE linkedin_stage
   URL = 's3://snowflake-lab-bucket/';
 
+-- Vérification : liste les fichiers disponibles dans le bucket
+-- On doit voir les 8 fichiers CSV et JSON du projet
 LIST @linkedin_stage;
