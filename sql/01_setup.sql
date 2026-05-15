60394edf27d50500cf0848be30467914d6ceed4c
JadeMR15 <jade.merle-remond@esme.fr>
2026-05-15 19:16:43 +0200

    Update 01_setup.sql

--- sql/01_setup.sql
+++ sql/01_setup.sql
@@ -1,5 +1,8 @@
+-- On crée la base de données principale du projet
 CREATE DATABASE IF NOT EXISTS LINKEDIN; 
 
+-- On crée les 3 couches de l'architecture Medallion
+-- Bronze = données brutes, Silver = données nettoyées, Gold = données analytiques
 CREATE SCHEMA IF NOT EXISTS LINKEDIN.BRONZE; 
 CREATE SCHEMA IF NOT EXISTS LINKEDIN.SILVER; 
 CREATE SCHEMA IF NOT EXISTS LINKEDIN.GOLD;
