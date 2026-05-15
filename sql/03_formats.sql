7d4010ca37fb993f5c75b0f96e37ea800f122f3e
JadeMR15 <jade.merle-remond@esme.fr>
2026-05-15 19:19:42 +0200

    Update 03_formats.sql

--- sql/03_formats.sql
+++ sql/03_formats.sql
@@ -1,5 +1,9 @@
 USE SCHEMA LINKEDIN.BRONZE;
 
+-- Format pour les fichiers CSV
+-- FIELD_OPTIONALLY_ENCLOSED_BY : gère les champs entre guillemets
+-- SKIP_HEADER : ignore la première ligne (c'est les noms des colonnes)
+-- NULL_IF : convertit les chaînes vides en valeurs nulles
 CREATE OR REPLACE FILE FORMAT csv_format
   TYPE = 'CSV'
   FIELD_OPTIONALLY_ENCLOSED_BY = '"'
@@ -7,6 +11,8 @@ CREATE OR REPLACE FILE FORMAT csv_format
   NULL_IF = ('', 'NULL')
   EMPTY_FIELD_AS_NULL = TRUE;
 
+-- Format pour les fichiers JSON
+-- STRIP_OUTER_ARRAY : nécessaire car les fichiers JSON commencent par un tableau et pas un objet 
 CREATE OR REPLACE FILE FORMAT json_format
   TYPE = 'JSON'
   STRIP_OUTER_ARRAY = TRUE;
